c8d6c6c16c432b6d6fb6daa84d60afec7c611498
Vanessa Fuangi <vanessafuangi@gmail.com>
2023-11-10 17:49:07 +0100

    Finished the login and forgot password pages, including all their styles and necessary routes

---
+++ src/routes/authentication/index.js
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import { Register,  SignIn, ForgotPassword } from "../../screens/authentication";
+
+function AuthRoute() {
+  return (
+    <Routes>
+      <Route path="/auth">
+        <Route path="register" element={< Register />} />
+        <Route path="signin" element={ <SignIn />} />
+        <Route path="forgot-password" element={ <ForgotPassword /> } />
+
+      </Route>
+    </Routes>
+  )
+}
+
+export default AuthRoute;
---
+++ src/screens/authentication/SignIn.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import AuthAnimator from "../../components/auth/AuthAnimator";
+
+function Signin() {
+  const [signInEmail, setSignInEmail] = useState("")
+  const [signInPassword, setSignInPassword] = useState ("")
+
+
+  function handleSignIn(e) {
+    e.preventDefault()
+    alert("Account Created")
+
+    // ? clear the form after the data has been submitted
+    setSignInEmail("")
+    setSignInPassword("")
+  }
+
+
+  return (
+    <div className="auth">
+      <AuthAnimator />
+
+      <div className="form">
+        <form onSubmit={handleSignIn}>
+          <h1>Login</h1>
+          <p>Enter your email and password to access your account</p>
+
+          <div className="email">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              value={signInEmail}
+              placeholder="your@email.com"
+              onChange={(e) => setSignInEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="password">
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={signInPassword}
+              onChange={(e) => setSignInPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="forgot">
+            <button>Login</button>
+            <a href="./forgot-password">Forgot Password?</a>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Signin
