e0c69960d864a1305f32fff635c588846e15b7bf
clintjeff2 <clintjeff2@gmail.com>
2023-12-01 16:57:31 +0100

    Added the course form for adding courses

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -2,3 +2,4 @@ export { default as StudentForm } from './StudentForm';
 export { default as TeacherForm } from './TeacherForm';
 export { default as DepartmentForm } from './DepartmentForm';
 export { default as SpecialtyForm } from './SpecialtyForm';
+export { default as CourseForm } from './CourseForm';
