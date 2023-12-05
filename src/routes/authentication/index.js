f12f7ec7c3c81f63dfb23157b401e9336b08e3cc
clintjeff2 <clintjeff2@gmail.com>
2023-12-05 17:22:55 +0100

    Modified the authentication route to avoid nesting with Route

--- src/routes/authentication/index.js
+++ src/routes/authentication/index.js
@@ -1,14 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import { SignIn, ForgotPassword} from '../../screens/authentication';
+import { SignIn, ForgotPassword } from '../../screens/authentication';
 
 function AuthRoute() {
 	return (
 		<Routes>
-			<Route path="/auth">
-				{/* <Route path="register" element={< Register />} /> */}
-				<Route path="signin" element={<SignIn />} />
-				<Route path="forgot-password" element={<ForgotPassword />} />
-			</Route>
+			{/* <Route path="register" element={< Register />} /> */}
+			<Route path="/auth/signin" element={<SignIn />} />
+			<Route path="/auth/forgot-password" element={<ForgotPassword />} />
 		</Routes>
 	);
 }
