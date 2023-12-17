5fac853492e902786d35577a28d6dea2cb570caa
clintjeff2 <clintjeff2@gmail.com>
2023-12-17 13:24:57 +0100

    made changes to reflect the testing branch

--- src/store/APIs/apiRequest.js
+++ src/store/APIs/apiRequest.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const apiRequest = async (method, url, data = '', cred = true) => {
-	url = `${process.env.REACT_APP_NODE_HOST_APP}:${process.env.REACT_APP_NODE_PORT}` + url;
+	url =
+		`${process.env.REACT_APP_NODE_HOST_APP}:${process.env.REACT_APP_NODE_PORT}` +
+		url;
 	console.log(url, cred);
 	return await axios({
 		method,
