caed0ed7828acf3e21297665e1f779d3c36d573a
clintjeff2 <clintjeff2@gmail.com>
2023-12-30 05:01:17 +0100

    Exported the marks add for premock and mock

--- src/screens/pages/marks/index.js
+++ src/screens/pages/marks/index.js
@@ -1,3 +1,5 @@
 export { default as MarksAddExam } from './MarksAddExam';
 export { default as MarksAddCA } from './MarksAddCA';
 export { default as MarkList } from './MarkList';
+export { default as MarksAddPreMock } from './MarksAddPreMock';
+export { default as MarksAddMock } from './MarksAddMock';
