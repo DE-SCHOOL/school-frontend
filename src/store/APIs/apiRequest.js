6fe79f9f7fb2c3b813783fd785742b7acfeaf9b6
clintjeff2 <clintjeff2@gmail.com>
2024-01-27 06:08:59 +0100

    App ready for deployment

--- src/store/APIs/apiRequest.js
+++ src/store/APIs/apiRequest.js
@@ -4,8 +4,7 @@ export const apiRequest = async (method, url, data = '', cred = true) => {
 	const token = JSON.parse(localStorage.getItem('loggedIn'));
 	const tokenID = token !== undefined && token ? '/' + token.token : '';
 
-	url =
-		`http://localhost:${process.env.REACT_APP_NODE_PORT}` + url + `${tokenID}`;
+	url = `${process.env.REACT_APP_NODE_HOST_APP}` + url + `${tokenID}`;
 	console.log(url, cred);
 	return await axios({
 		method,
