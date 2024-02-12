d9ba7033dc036de7ebb256a065f456b00d32f85f
clintjeff2 <clintjeff2@gmail.com>
2024-02-12 06:24:14 +0100

    Added the loading spinner to show when editing asigned staffs to courses

--- src/screens/pages/staff courses/EditAsignedStaffCourses.jsx
+++ src/screens/pages/staff courses/EditAsignedStaffCourses.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Layout, SectionIntro } from '../../../components/layout';
 import { StaffCourseFormEdit } from '../../../components/form';
+import { useSelector } from 'react-redux';
+import Failure from '../../../components/signal/Failure';
+import Loader from '../../../components/loaders/Loader';
 
 function EditAsignedStaffCourses() {
+	const load = useSelector((state) => state.dashboard);
 	return (
 		<Layout>
 			<SectionIntro title="Edit assigned courses" main="review" sub="assign" />
@@ -10,6 +14,10 @@ function EditAsignedStaffCourses() {
 				<h2 className="header-secondary">Assigning courses</h2>
 				<StaffCourseFormEdit styles="mg-top-md" />
 			</section>
+			{load.error === true && load.errorMessage !== '' && (
+				<Failure message={load.errorMessage} />
+			)}
+			{load.isLoading && <Loader />}
 		</Layout>
 	);
 }
