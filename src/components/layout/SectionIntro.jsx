cdfd41ed9c2d8158d9b8bbd571f5677d83311a3e
clintjeff2 <clintjeff2@gmail.com>
2024-03-02 21:26:38 +0100

    Added a title to the marks printing page to show that the course marks belongs to a particular semester

--- src/components/layout/SectionIntro.jsx
+++ src/components/layout/SectionIntro.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { semester } from '../../utilities/periodInfo';
 
 //Styled in the the layout scss file in component styling
 
 function SectionIntro({ title, main, sub }) {
+	const sem = semester();
 	return (
 		<div className="section-intro">
 			<h1 className="header-primary">{title}</h1>
 			<h2 className="header-secondary">
+				<span className="main sem">
+					{sem === 's1' ? 'First' : 'Second'} Semester{' '}
+				</span>
 				<span className="main">{main}</span>
 				<span> / </span>
 				<span className="sub">{sub}</span>
