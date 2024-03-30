1151088666d98dc4b9da501af2539ff22934016a
clintjeff2 <clintjeff2@gmail.com>
2024-03-30 19:10:16 +0100

    Exported AllSTaffReview, StaffReview, and StaffReviewCourses as named exports

---
+++ src/screens/pages/human resource/index.js
@@ -0,0 +1,3 @@
+export { default as AllStaffReview } from './AllStaffReview';
+export { default as StaffReview } from './StaffReview';
+export { default as StaffReviewCourses } from './StaffReviewCourses';
