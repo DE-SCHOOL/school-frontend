7e6b524fa17bf218cea9363fbeaf130d601c7d89
clintjeff2 <clintjeff2@gmail.com>
2024-04-04 19:29:53 +0100

    Created the the poll's directory for the poll's routes

---
+++ src/routes/pages/polls/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+//IMPORTING the different pages
+import { ProgramEdit } from './../../../screens/pages/programs';
+
+//importing a protector component
+import Protected from '../../../components/auth/Protected';
+
+// importing different rights
+import * as RIGHTS from './../../../utilities/restrict';
+import PollAdd from '../../../screens/pages/polls/PollAdd';
+import PollList from '../../../screens/pages/polls/PollList';
+function ProgramRoute() {
+	return (
+		<Routes>
+			<Route
+				path="/poll/add"
+				element={
+					<Protected restrict={RIGHTS.TO_ALL_OFFICE_STAFF}>
+						<PollAdd />
+					</Protected>
+				}
+			/>
+			<Route
+				path="/poll/list"
+				element={
+					<Protected restrict={RIGHTS.TO_ALL_STAFF}>
+						<PollList />
+					</Protected>
+				}
+			/>
+			<Route
+				path="/poll/edit/:id"
+				element={
+					<Protected restrict={RIGHTS.TO_ALL_OFFICE_STAFF}>
+						<ProgramEdit />
+					</Protected>
+				}
+			/>
+		</Routes>
+	);
+}
+
+export default ProgramRoute;
