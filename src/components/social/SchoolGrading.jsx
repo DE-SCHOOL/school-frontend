ce398307fcb5ed5534b027e6d094a3afc617ca08
clintjeff2 <clintjeff2@gmail.com>
2024-07-17 16:02:38 +0100

    Displayed REGISTRAR below Registrar's name

--- src/components/social/SchoolGrading.jsx
+++ src/components/social/SchoolGrading.jsx
@@ -5,7 +5,7 @@ function SchoolGrading() {
 	return (
 		<div className="total-gpa school-grading">
 			<div className="registrar">
-				<span>Nfor Frederick, PhD</span>
+				<span>Nfor Frederick, PhD</span> <br />
 				<span>REGISTRAR</span>
 			</div>
 			<table className="results-total mg-top">
