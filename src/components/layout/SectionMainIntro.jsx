22c59f4526a81e7e75f087d8b18b4cd005408c57
clintjeff2 <clintjeff2@gmail.com>
2024-08-15 12:30:37 +0100

    Added a promote all button

--- src/components/layout/SectionMainIntro.jsx
+++ src/components/layout/SectionMainIntro.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FaArrowDown, FaPlus } from 'react-icons/fa6';
+import { GiUpgrade } from 'react-icons/gi';
+
 import { BsRepeat } from 'react-icons/bs';
 import { useLocation } from 'react-router-dom';
 
@@ -9,7 +11,7 @@ import { Link } from 'react-router-dom';
 //Styled in the layout sass file in components stying
 
 //Sections name, download button, refresh button and Add button
-function SectionMainIntro({ title, styles, link }) {
+function SectionMainIntro({ title, styles, link, promotion = false, ftn }) {
 	const loc = useLocation();
 	const handleRefresh = () => {
 		window.location = loc.pathname;
@@ -18,24 +20,41 @@ function SectionMainIntro({ title, styles, link }) {
 	return (
 		<div className={`section-main-intro ${styles ? styles : ''}`}>
 			<h2 className="header-secondary">{title}</h2>
-			<div className="actions">
-				<button className="button-main button-main-small">
-					<FaArrowDown />
-					<span className="text">Download</span>
-				</button>
-				<Link to={link}>
+			{promotion === false && (
+				<div className="actions">
 					<button className="button-main button-main-small">
-						<FaPlus />
-						<span className="text">Add</span>
+						<FaArrowDown />
+						<span className="text">Download</span>
 					</button>
-				</Link>
-				<button
-					className="button-main button-main-small"
-					onClick={handleRefresh}
-				>
-					<BsRepeat />
-				</button>
-			</div>
+					<Link to={link}>
+						<button className="button-main button-main-small">
+							<FaPlus />
+							<span className="text">Add</span>
+						</button>
+					</Link>
+					<button
+						className="button-main button-main-small"
+						onClick={handleRefresh}
+					>
+						<BsRepeat />
+					</button>
+				</div>
+			)}
+			{promotion === true && (
+				<div className="actions">
+					<button className="button-main button-main-small" onClick={ftn}>
+						<GiUpgrade />
+						<span className="text">Promote All</span>
+					</button>
+
+					<button
+						className="button-main button-main-small"
+						onClick={handleRefresh}
+					>
+						<BsRepeat />
+					</button>
+				</div>
+			)}
 		</div>
 	);
 }
