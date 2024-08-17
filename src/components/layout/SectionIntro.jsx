68ccf4f4c287b54c494159be298b5144d92b4769
clintjeff2 <clintjeff2@gmail.com>
2024-08-17 16:36:01 +0100

    Renamed title for section intro,mostly used for mark sheet title to have academic year information e.g Cloud Computing 2020/2021

--- src/components/layout/SectionIntro.jsx
+++ src/components/layout/SectionIntro.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { semester } from '../../utilities/periodInfo';
-
+import { useSelector } from 'react-redux';
 //Styled in the the layout scss file in component styling
 
 function SectionIntro({ title, main, sub }) {
 	const sem = semester();
+	const academicYear = useSelector((state) => state.years.currentYear);
 	return (
 		<div className="section-intro">
 			<h1 className="header-primary">{title}</h1>
 			<h2 className="header-secondary">
 				<span className="main sem">
-					{sem === 's1' ? 'First' : 'Second'} Semester{' '}
+					{sem === 's1' ? 'First' : 'Second'}{' '}
+					{academicYear?._id !== undefined &&
+						` Semester ${academicYear?.schoolYear}`}
 				</span>
 				<span className="main">{main}</span>
 				<span> / </span>
