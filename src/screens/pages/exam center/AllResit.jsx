9588eca97f6964037df6e906d25df77b50e4e4e0
clintjeff2 <clintjeff2@gmail.com>
2024-08-17 16:56:06 +0100

    Incoporated current academic year to get resit only for current academic year

--- src/screens/pages/exam center/AllResit.jsx
+++ src/screens/pages/exam center/AllResit.jsx
@@ -6,18 +6,27 @@ import Loader from '../../../components/loaders/Loader';
 import Button from '../../../components/buttons/Button';
 import TableAllResit from '../../../components/tables/TableAllResit';
 import { semester } from '../../../utilities/periodInfo';
+import { getCurrentYear } from '../../../store/academic year/academicYearSlice';
 
 function PrintList() {
 	//Defining the dispatch function, and the useSelector to get students data
 	const dispatch = useDispatch();
 	const students = useSelector((state) => state.exams.studentResit);
+	const academicYear = useSelector((state) => state.years.currentYear);
 	const load = useSelector((state) => state.exams);
 	const [scroll, setScroll] = useState(0);
 
 	//useEffect to dispatch student data after initial render
 	useEffect(() => {
-		dispatch(getStudentResit({ semester: semester() }));
-	}, [dispatch]);
+		dispatch(getCurrentYear());
+		if (academicYear?._id !== undefined)
+			dispatch(
+				getStudentResit({
+					semester: semester(),
+					academicYear: academicYear?.schoolYear,
+				})
+			);
+	}, [dispatch, academicYear?._id]);
 
 	window.onscroll = () => {
 		if (window.scrollY > 200) {
@@ -26,7 +35,6 @@ function PrintList() {
 			setScroll(0);
 		}
 	};
-
 
 	return (
 		<div className="stud-print">
