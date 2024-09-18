2376b05dbe51e52a960f3f017796bd68a3fdf145
clintjeff2 <clintjeff2@gmail.com>
2024-09-19 00:48:19 +0100

    Configured component to take new props and work well for deleting student from a particular academic year

--- src/components/mod/DeleteModal.jsx
+++ src/components/mod/DeleteModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { defaultDeleteEntity } from '../../store/ui-state/ui-stateSlice';
 import { deleteStudent } from '../../store/students/studentSlice';
 import { deleteStaff } from '../../store/staffs/staffSlice';
@@ -7,23 +7,51 @@ import { deleteSpecialty } from '../../store/specialty/specialtySlice';
 import { deleteCourse } from '../../store/courses/courseSlice';
 import { deleteDepartment } from '../../store/departments/departmentSlice';
 import { deleteProgram } from '../../store/program/programSlice';
+import { deleteQuestion } from '../../store/question/questionSlice';
+import {
+	deletePromotedStudent,
+	getStudentPerAcademicYearNextStudents,
+} from '../../store/academic year/academicYearSlice';
 
-function DeleteModal({ name, id, type }) {
+function DeleteModal({
+	name,
+	id,
+	type,
+	nextAcademicYear = '',
+	newClass = null,
+}) {
 	const [modalText, setModalText] = useState('');
+	const year = useSelector((state) => state.years.currentYear);
 	const dispatch = useDispatch();
-	console.log(modalText, id);
+	// console.log(modalText, id);
 	const handleRemoveModal = () => {
 		dispatch(defaultDeleteEntity());
 	};
 
 	window.onclick = (e) => {
-		console.log(e.target.id);
+		// console.log(e.target.id);
 		if (e.target.id === 'outer') {
 			handleRemoveModal();
 		}
 	};
+	let reactDeleteElement = (
+		<div className="modal-text">
+			Note: <span>{name}</span> is about to be deleted from the database. After
+			this delete, the action would be irreversible. Type
+			<span> {id}</span> in the field below to confirm deletion.
+		</div>
+	);
+	if (type === 'promotion')
+		reactDeleteElement = (
+			<div className="modal-text">
+				Note: <span>{name}</span> is about to be deleted from the
+				{' ' + nextAcademicYear?.schoolYear} academic year. After this delete,
+				the action would be irreversible. Type
+				<span> {id}</span> in the field below to confirm deletion.
+			</div>
+		);
 
-	const handleDeleteEntity = () => {
+	const handleDeleteEntity = async () => {
 		// alert('I would do the deletion');
 		if (type === 'student') {
 			dispatch(deleteStudent({ id }));
@@ -37,6 +65,26 @@ function DeleteModal({ name, id, type }) {
 			dispatch(deleteDepartment({ id }));
 		} else if (type === 'program') {
 			dispatch(deleteProgram({ id }));
+		} else if (type === 'question') {
+			dispatch(deleteQuestion({ id }));
+		} else if (type === 'promotion') {
+			console.log({
+				studentID: id,
+				nextAcademicYearID: nextAcademicYear?._id,
+				newClass,
+				currentYearID: year?._id,
+			});
+			await dispatch(
+				deletePromotedStudent({
+					studentID: id,
+					nextAcademicYearID: nextAcademicYear?._id,
+					newClass,
+					currentYearID: year?._id,
+				})
+			);
+			await dispatch(
+				getStudentPerAcademicYearNextStudents({ _id: nextAcademicYear?._id })
+			);
 		}
 
 		handleRemoveModal();
@@ -44,11 +92,7 @@ function DeleteModal({ name, id, type }) {
 	return (
 		<div className={'mo'} id="outer">
 			<div className="modal">
-				<div className="modal-text">
-					Note: <span>{name}</span> is about to be deleted from the database.
-					After this delete, the action would be irreversible. Type
-					<span> {id}</span> in the field below to confirm deletion.
-				</div>
+				{reactDeleteElement}
 				<div className="input">
 					<input
 						type="text"
