8280f39dd195a749c205c3d5be51b0008f0f9693
clintjeff2 <clintjeff2@gmail.com>
2024-11-16 14:18:57 +0100

    Added function to add, and update acadmic term

--- src/utilities/periodInfo.js
+++ src/utilities/periodInfo.js
@@ -12,6 +12,20 @@ export const semester = () => {
 	return period.current;
 };
 
+//Term
+export const academicTerm = () => {
+	let period = JSON.parse(localStorage.getItem('term')) || undefined;
+	// console.log(period, 11);
+
+	//if term is not defined in local storage, set it to term 1
+	if (period === undefined) {
+		period = { current: 't1' };
+		localStorage.setItem('term', JSON.stringify(period));
+	}
+	// console.log(period, 22);
+	return period.current;
+};
+
 export const year = () => {
 	const period = JSON.parse(localStorage.getItem('academicYear'));
 
