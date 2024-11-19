eb68b70fce69ea9d9a3caaacde3cd2c99a7f862c
clintjeff2 <clintjeff2@gmail.com>
2024-11-19 18:51:37 +0100

    Exported result term and sequence pages as named exports

--- src/screens/pages/exam center/index.js
+++ src/screens/pages/exam center/index.js
@@ -1,4 +1,5 @@
 export { default as StudentsMarks } from './StudentsMarks';
-export { default as StudentResult } from './StudentResult';
+export { default as StudentResultTerm } from './StudentResultTerm';
+export { default as StudentResultSequence } from './StudentResultSequence';
 export { default as StudentResultAllTerm } from './StudentResultAllTerm';
 export { default as StudentResultAllSequence } from './StudentResultAllSequence';
