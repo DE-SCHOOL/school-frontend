840e282e438f8debadbc0edcd3c41d97c6d4f962
clintjeff2 <clintjeff2@gmail.com>
2024-11-19 13:24:50 +0100

    Created the report card download page for term and sequence

---
+++ src/screens/pages/exam center/StudentResultAllSequence.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import SearchStudents from '../../../components/search/SearchStudents';
+import { getStudentsExam } from '../../../store/exams/examSlice';
+
+import Loader from '../../../components/loaders/Loader';
+import Button from '../../../components/buttons/Button';
+import { TableAllResultDataSequence } from '../../../components/tables';
+import Failure from '../../../components/signal/Failure';
+import { getCurrentYear } from '../../../store/academic year/academicYearSlice';
+import { getAllStudentsPerAcademicYear } from '../../../store/students/studentSlice';
+
+function StudentResultAllSequence() {
+	//Defining the dispatch function, and the useSelector to get students data
+	const dispatch = useDispatch();
+	// const students = useSelector((state) => state.exams.students);
+	const load = useSelector((state) => state.marks);
+	const academicYear = useSelector((state) => state.years.currentYear);
+	const [scroll, setScroll] = useState(0);
+
+	// console.log(students, students.length);
+	//useEffect to dispatch student data after initial render
+	useEffect(() => {
+		if (academicYear?._id !== undefined) {
+			dispatch(getStudentsExam(academicYear?._id));
+			dispatch(getAllStudentsPerAcademicYear(academicYear?._id));
+		}
+	}, [dispatch, academicYear?._id]);
+
+	useEffect(() => {
+		dispatch(getCurrentYear());
+	}, [dispatch]);
+
+	window.onscroll = () => {
+		if (window.scrollY > 200) {
+			setScroll(1);
+		} else {
+			setScroll(0);
+		}
+	};
+
+	return (
+		<div className="stud-print">
+			<SearchStudents
+				styles={'mg-top-md mg-bt-md'}
+				type="print"
+				form="STUDENT RESULTS"
+			/>
+
+			<section className="students">
+				<TableAllResultDataSequence styles="no-position" />
+			</section>
+			<Button styles={scroll} />
+			{load.isLoading && <Loader />}
+			{load.error === true && load.errorMessage && (
+				<Failure message={load.errorMessage} />
+			)}
+			{/* {load.error === false && setStaffData(defaultInfo)} */}
+		</div>
+	);
+}
+
+export default StudentResultAllSequence;
