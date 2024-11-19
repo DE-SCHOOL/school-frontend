516614d84afbaca2a8b044fca07609fa7e3859f6
clintjeff2 <clintjeff2@gmail.com>
2024-11-19 17:57:09 +0100

    Added useEffect function to get all students results even though I need just a particular student's own, this further helps to calculate that particular students result

--- src/screens/pages/exam center/StudentResult.jsx
+++ src/screens/pages/exam center/StudentResult.jsx
@@ -6,26 +6,61 @@ import { FaArrowDown } from 'react-icons/fa';
 
 //importing slices
 import StudentInfo from '../../../components/social/StudentInfo';
-import { getStudent } from '../../../store/exams/examSlice';
+import {
+	getStudent,
+	getStudentsPerSearch,
+} from '../../../store/exams/examSlice';
 
 //importing components
 import { TableResults } from './../../../components/tables';
 import { Layout } from '../../../components/layout';
 import Loader from './../../../components/loaders/Loader';
 import Failure from './../../../components/signal/Failure';
+import { getAllStudentMarkSheetAllCourses } from '../../../store/marks/markSlice';
 
 function StudentResult() {
 	const dispatch = useDispatch();
 	const student = useSelector((stud) => stud.exams.student);
+	const students = useSelector((stud) => stud.exams.students);
 	const year = useSelector((state) => state.years.currentYear);
 	const exams = useSelector((stud) => stud.exams);
 
 	const params = useParams();
 
 	useEffect(() => {
-		if (year?._id !== undefined)
+		if (year?._id !== undefined) {
 			dispatch(getStudent({ id: params.studentID, academicYearID: year?._id }));
+		}
 	}, [dispatch, params.studentID, year?._id]);
+
+	useEffect(() => {
+		if (student !== undefined && Object.entries(student).length !== 0)
+			dispatch(getStudentsPerSearch({ specialty: student.specialty?._id }));
+	}, [student, dispatch]);
+
+	useEffect(() => {
+		if (year?._id !== undefined) {
+			//Get the students whose results are to be displayed
+
+			// const academicYear = '2023/2024';
+
+			//getting the student IDs
+			let studIDs = [];
+			students.map((student) => {
+				studIDs.push(student._id);
+				return student;
+			});
+
+			//searching  data
+			const searchData = {
+				academicYear: year?.schoolYear,
+				students: studIDs,
+			};
+			dispatch(getAllStudentMarkSheetAllCourses(searchData));
+		}
+
+		//eslint-disable-next-line
+	}, [students?.length, year?._id]);
 	return (
 		<Layout>
 			{student !== undefined && Object.entries(student).length !== 0 && (
