366127f5286c42480bd0477340d8863754ef47b8
clintjeff2 <clintjeff2@gmail.com>
2024-11-21 03:33:51 +0100

    Exported the results all year page as a named export

--- src/screens/pages/exam center/index.js
+++ src/screens/pages/exam center/index.js
@@ -1,5 +1,6 @@
-export { default as StudentsMarks } from './StudentsMarks';
+export { default as StudentsResults } from './StudentsResults';
 export { default as StudentResultTerm } from './StudentResultTerm';
 export { default as StudentResultSequence } from './StudentResultSequence';
 export { default as StudentResultAllTerm } from './StudentResultAllTerm';
 export { default as StudentResultAllSequence } from './StudentResultAllSequence';
+export { default as StudentsResultsYear } from './StudentsResultsYear';
