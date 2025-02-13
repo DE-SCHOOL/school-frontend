fca637c8813a7040698798afc92581a5f8b1c629
clintjeff2 <clintjeff2@gmail.com>
2025-02-13 10:52:28 +0100

    Added responsiveness for my courses table

--- src/screens/pages/courses/MyCourses.jsx
+++ src/screens/pages/courses/MyCourses.jsx
@@ -67,16 +67,18 @@ function MyCourses() {
 
 				{/* Show student table information only if students data has loaded */}
 				{courses !== undefined && courses?.courses?.length !== 0 && (
-					<TableCourses
-						styles="mg-top"
-						// parse student data, or student searched data in case a search was performed
-						tableData={
-							coursesState.length !== 0 ? coursesState : courses.courses
-						}
-						header={courseHeader}
-						paggingNum={numPages}
-						tableType="personal"
-					/>
+					<div className="main-table-container">
+						<TableCourses
+							styles="mg-top"
+							// parse student data, or student searched data in case a search was performed
+							tableData={
+								coursesState.length !== 0 ? coursesState : courses.courses
+							}
+							header={courseHeader}
+							paggingNum={numPages}
+							tableType="personal"
+						/>
+					</div>
 				)}
 
 				{/* Show student table information only if students data has loaded */}
