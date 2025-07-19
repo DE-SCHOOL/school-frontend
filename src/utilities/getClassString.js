e48c0bfc891733986209b13cc5c947e8117e47c1
clintjeff2 <clintjeff2@gmail.com>
2025-07-19 14:18:28 +0100

    Refactor getClassString utility: cleaner mapping and default fallback

--- src/utilities/getClassString.js
+++ src/utilities/getClassString.js
@@ -2,25 +2,28 @@ export function returnClassString(level) {
 	let levelString = '';
 	switch (level) {
 		case 100:
-			levelString = 'Form 1';
+			levelString = 'Nursery 1';
 			break;
 		case 200:
-			levelString = 'Form 2';
+			levelString = 'Nursery 2';
 			break;
 		case 300:
-			levelString = 'Form 3';
+			levelString = 'Class 1';
 			break;
 		case 400:
-			levelString = 'Form 4';
+			levelString = 'Class 2';
 			break;
 		case 500:
-			levelString = 'Form 5';
+			levelString = 'Class 3';
 			break;
 		case 601:
-			levelString = 'Lower Sixth';
+			levelString = 'Class 4';
 			break;
 		case 602:
-			levelString = 'Upper Sixth';
+			levelString = 'Class 5';
+			break;
+		case 603:
+			levelString = 'Class 6';
 			break;
 		default:
 			levelString = 'Promoted';
